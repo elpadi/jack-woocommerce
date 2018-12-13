602fffd1663b49e9f0a15ae8faf7128cca1f389e
carlos (minimal) <padi_05@yahoo.com>
2018-12-13 18:46:25 -0500

    Split header css. Added logo

--- wp-content/themes/jack/functions.php
+++ wp-content/themes/jack/functions.php
@@ -9,6 +9,16 @@ add_action('wp_enqueue_scripts', function() {
 
 	$assets = new Assets(get_stylesheet_directory_uri(), __DIR__, 'assets/src');
 	$assets->css('base/overrides');
-	$assets->css('areas/header');
+	$assets->css('header/header');
+	$assets->css('header/nav');
+	
+});
 
+add_filter('theme_mod_custom_logo', function($value) {
+	return 1;
+});
+
+add_filter('get_custom_logo', function($html) {
+	$end = '</a>';
+	return str_replace($end, sprintf('<img src="%s" alt="%s">', get_stylesheet_directory_uri().'/assets/icons/logo.svg', get_bloginfo('name')).$end, $html);
 });
