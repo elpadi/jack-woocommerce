801fc1b13e4a6e035e12385b32b87751a9bf132a
carlos (minimal) <padi_05@yahoo.com>
2018-12-14 21:08:05 -0500

    Single product template

--- wp-content/themes/jack/functions.php
+++ wp-content/themes/jack/functions.php
@@ -13,14 +13,26 @@ add_action('wp_enqueue_scripts', function() {
 	$assets->css('header/nav');
 	$assets->css('content/content');
 	$assets->css('content/product-grid');
+	$assets->css('content/product');
 	
 });
 
-add_filter('theme_mod_custom_logo', function($value) {
-	return 1;
-});
+if (is_admin() == false) {
 
-add_filter('get_custom_logo', function($html) {
-	$end = '</a>';
-	return str_replace($end, sprintf('<img src="%s" alt="%s">', get_stylesheet_directory_uri().'/assets/icons/logo.svg', get_bloginfo('name')).$end, $html);
-});
+	add_filter('theme_mod_custom_logo', function($value) {
+		return 1;
+	});
+
+	add_filter('get_custom_logo', function($html) {
+		$end = '</a>';
+		return str_replace($end, sprintf('<img src="%s" alt="%s">', get_stylesheet_directory_uri().'/assets/icons/logo.svg', get_bloginfo('name')).$end, $html);
+	});
+
+	add_filter('the_title', function($title) {
+		$parts = explode(' &#8211; ', $title);
+		if (count($parts) == 1) return $title;
+		return sprintf('%s<span>%s</span>', $parts[0], implode('', array_slice($parts, 1)));
+	});
+
+}
+
